1fab5655062ffc64e9b36efdb26565dee5f0ebd1
Vadim Spivak <thevadimspivak@gmail.com>
2023-09-15 12:29:35 +0500

    15.09

---
+++ problems/A/1A.py
@@ -0,0 +1,10 @@
+n, m, a = map(int, input().split())
+
+# Находим количество секций по горизонтали и вертикали
+sections_horizontal = (n + a - 1) // a
+sections_vertical = (m + a - 1) // a
+
+# Находим общее количество плит
+total_tiles = sections_horizontal * sections_vertical
+
+print(total_tiles)
